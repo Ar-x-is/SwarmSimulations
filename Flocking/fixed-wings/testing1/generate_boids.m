949e3f3364b9bbbf85e3700a33a0d7f368ae8979
Ar-x-is <210260008@iitb.ac.in>
2024-11-07 13:40:08 +0530

    deleted some old folders

--- Flocking/fixed-wings/testing1/generate_boids.m
+++ Flocking/fixed-wings/testing1/generate_boids.m
@@ -1,8 +1,8 @@
 function [positions, velocities] = generate_boids(num_boids, range, min_d)
     % Check if the range is too small for the specified number of boids
-    if range / sqrt(num_boids) < min_d
-        error('The specified range is too small to accommodate the specified number of boids with the given minimum distance.');
-    end
+    % if range / sqrt(num_boids) < min_d
+    %     error('The specified range is too small to accommodate the specified number of boids with the given minimum distance.');
+    % end
     
     % Initialize the boids' positions and velocities
     positions = zeros(num_boids, 2);
@@ -11,33 +11,34 @@ function [positions, velocities] = generate_boids(num_boids, range, min_d)
     % Generate boids with minimum distance constraint
     for i = 1:num_boids
         % Generate a position until it satisfies the minimum distance constraint
-        while true
-            position = range*rand(1, 2); % Random position between [0, range]
-            % Check if the new position satisfies the minimum distance constraint with existing positions
-            if all(pdist2(position, positions) >= min_d)
-                positions(i, :) = position;
-                break; % Exit the loop if the condition is satisfied
-            end
-        end
+        % while true
+        % position = range*rand(1, 2); % Random position between [0, range]
+        %     % Check if the new position satisfies the minimum distance constraint with existing positions
+        %     if all(pdist2(position, positions) >= min_d)
+        %         positions(i, :) = position;
+        %         break; % Exit the loop if the condition is satisfied
+        %     end
+        % end
+        % positions(i,:) = range*rand(1,2);
     end
         % Assign positions to be the vertices of a triangular/square lattice
          % Calculate the dimensions of the grid
     side_length = ceil(sqrt(num_boids));
 
-    % % Place agents on the grid
-    % idx = 1;
-    % for i = 0:side_length-1
-    %     for j = 0:side_length-1
-    %         if idx > num_boids
-    %             break;
-    %         end
-    %         positions(idx, :) = [i * min_d, j * min_d];
-    %         idx = idx + 1;
-    %     end
-    %     if idx > num_boids
-    %         break;
-    %     end
-    % end
+    % Place agents on the grid
+    idx = 1;
+    for i = 0:side_length-1
+        for j = 0:side_length-1
+            if idx > num_boids
+                break;
+            end
+            positions(idx, :) = [i * min_d, j * min_d];
+            idx = idx + 1;
+        end
+        if idx > num_boids
+            break;
+        end
+    end
     % Assign random velocities [-range/2, range/2]
     for i = 1:num_boids
         velocities(i, :) = range*rand(1, 2) - range/2;
--- Flocking/fixed-wings/testing2/rotate.m
+++
@@ -1,8 +0,0 @@
-function rotated_vector = rotate(vector, angle)
-    rotated_vector = zeros(size(vector));
-    % angle in radians
-    for i = 1:size(angle,1)
-        M = [cos(angle(i)), sin(angle(i)); -sin(angle(i)), cos(angle(i))];
-        rotated_vector(i,:) = (M * vector(i,:)')';
-    end
-end
